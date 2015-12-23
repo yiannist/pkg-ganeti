cfb1112ca02abd78bdd03d1f03aeebcf7224adb3
Apollon Oikonomopoulos <apoikos@debian.org>
2015-12-23 11:19:51 +0200

    Add missing QA files
    These files are not present in the upstream tarball, so import them from
    git  under debian/tests. Note that this is needed only on source
    versions using upstream tarballs; after switching to upstream git, all
    QA files are present in the Debian sources.

---
+++ debian/tests/qa/qa_iptables.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python -u
+#
+
+# Copyright (C) 2013 Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Manipulates nodes using `iptables` to simulate non-standard network
+conditions.
+
+"""
+
+import uuid
+
+import qa_config
+import qa_utils
+
+from qa_utils import AssertCommand
+
+# String used as a comment for produced `iptables` results
+IPTABLES_COMMENT_MARKER = "ganeti_qa_script"
+
+
+class RulesContext(object):
+  def __init__(self, nodes):
+    self._nodes = set()
+
+  def __enter__(self):
+    self._marker = IPTABLES_COMMENT_MARKER + "_" + str(uuid.uuid4())
+    return Rules(self)
+
+  def __exit__(self, ext_type, exc_val, exc_tb):
+    CleanRules(self._nodes, self._marker)
+
+  def _AddNode(self, node):
+    self._nodes.add(node)
+
+
+class Rules(object):
+  """Allows to introduce iptable rules and dispose them at the end of a block.
+
+  Don't instantiate this class directly. Use `with RulesContext() as r` instead.
+  """
+
+  def __init__(self, ctx=None):
+    self._ctx = ctx
+    if self._ctx is not None:
+      self._marker = self._ctx._marker
+    else:
+      self._marker = IPTABLES_COMMENT_MARKER
+
+  def _AddNode(self, node):
+    if self._ctx is not None:
+      self._ctx._AddNode(node)
+
+  def AppendRule(self, node, chain, rule, table="filter"):
+    """Appends an `iptables` rule to a given node
+    """
+    AssertCommand(["iptables", "-t", table, "-A", chain] +
+                  rule +
+                  ["-m", "comment",
+                   "--comment", self._marker],
+                  node=node)
+    self._AddNode(node)
+
+  def RedirectPort(self, node, host, port, new_port):
+    """Adds a rule to a master node that makes a destination host+port visible
+    under a different port number.
+
+    """
+    self.AppendRule(node, "OUTPUT",
+                    ["--protocol", "tcp",
+                     "--destination", host, "--dport", str(port),
+                     "--jump", "DNAT",
+                     "--to-destination", ":" + str(new_port)],
+                    table="nat")
+
+
+GLOBAL_RULES = Rules()
+
+
+def CleanRules(nodes, marker=IPTABLES_COMMENT_MARKER):
+  """Removes all QA `iptables` rules matching a given marker from a given node.
+
+  If no marker is given, the global default is used, which clean all custom
+  markers.
+  """
+  if not hasattr(nodes, '__iter__'):
+    nodes = [nodes]
+  for node in nodes:
+    AssertCommand(("iptables-save | grep -v '%s' | iptables-restore" %
+                    (marker, )),
+                  node=node)
---
+++ debian/tests/qa/qa_network.py
@@ -0,0 +1,122 @@
+#
+#
+
+# Copyright (C) 2013 Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+"""QA tests for networks.
+
+"""
+
+import qa_config
+import qa_tags
+import qa_utils
+
+from ganeti import query
+
+from qa_utils import AssertCommand
+
+
+def TestNetworkList():
+  """gnt-network list"""
+  qa_utils.GenericQueryTest("gnt-network", query.NETWORK_FIELDS.keys())
+
+
+def TestNetworkListFields():
+  """gnt-network list-fields"""
+  qa_utils.GenericQueryFieldsTest("gnt-network", query.NETWORK_FIELDS.keys())
+
+
+def GetNonexistentNetworks(count):
+  """Gets network names which shouldn't exist on the cluster.
+
+  @param count: Number of networks to get
+  @rtype: integer
+
+  """
+  return qa_utils.GetNonexistentEntityNames(count, "networks", "network")
+
+
+def TestNetworkAddRemove():
+  """gnt-network add/remove"""
+  (network1, network2) = GetNonexistentNetworks(2)
+
+  # Add some networks of different sizes.
+  # Note: Using RFC5737 addresses.
+  AssertCommand(["gnt-network", "add", "--network", "192.0.2.0/30", network1])
+  AssertCommand(["gnt-network", "add", "--network", "198.51.100.0/24",
+                 network2])
+  # Try to add a network with an existing name.
+  AssertCommand(["gnt-network", "add", "--network", "203.0.133.0/24", network2],
+                fail=True)
+
+  TestNetworkList()
+  TestNetworkListFields()
+
+  AssertCommand(["gnt-network", "remove", network1])
+  AssertCommand(["gnt-network", "remove", network2])
+
+  TestNetworkList()
+
+
+def TestNetworkTags():
+  """gnt-network tags"""
+  (network, ) = GetNonexistentNetworks(1)
+  AssertCommand(["gnt-network", "add", "--network", "192.0.2.0/30", network])
+  qa_tags.TestNetworkTags(network)
+  AssertCommand(["gnt-network", "remove", network])
+
+
+def TestNetworkConnect():
+  """gnt-network connect/disconnect"""
+  (group1, ) = qa_utils.GetNonexistentGroups(1)
+  (network1, ) = GetNonexistentNetworks(1)
+
+  default_mode = "bridged"
+  default_link = "xen-br0"
+  nicparams = qa_config.get("default-nicparams")
+  if nicparams:
+    mode = nicparams.get("mode", default_mode)
+    link = nicparams.get("link", default_link)
+  else:
+    mode = default_mode
+    link = default_link
+
+  nicparams = "mode=%s,link=%s" % (mode, link)
+
+  AssertCommand(["gnt-group", "add", group1])
+  AssertCommand(["gnt-network", "add", "--network", "192.0.2.0/24", network1])
+
+  AssertCommand(["gnt-network", "connect", "--nic-parameters", nicparams,
+                network1, group1])
+
+  TestNetworkList()
+
+  AssertCommand(["gnt-network", "disconnect", network1, group1])
+
+  AssertCommand(["gnt-group", "remove", group1])
+  AssertCommand(["gnt-network", "remove", network1])
